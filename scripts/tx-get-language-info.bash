7183aed06f6c72e9bc4f947ab60153d6814f934d
ehom <erwin.hom@puppet.com>
2019-02-19 01:57:50 -0800

    accept input argument as lang-id

--- scripts/tx-get-language-info.bash
+++ scripts/tx-get-language-info.bash
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 
-URL=https://www.transifex.com/api/2/language/$1/
+function usage {
+  echo "Usage: $(basename $0) xx-XX (language-identifier)"
+  exit 1
+}
 
-curl --include --location \
-  --config tx-account-info.txt \
-  --get $URL
+function get_lang_info {
+  URL=https://www.transifex.com/api/2/language/$1/
 
-echo
+  curl --include --location --config tx-account-info.txt \
+    --get $URL
+
+  echo
+}
+
+function valid_lang_id {
+  [[ $1 =~ ^[a-z]{2}[_|-][A-Z]{2}$ ]]
+}
+
+function main {
+  if [[ "$#" -eq 1 ]] && valid_lang_id $1
+  then
+    get_lang_info $1
+  else
+    usage
+  fi
+}
+
+main $1
+
